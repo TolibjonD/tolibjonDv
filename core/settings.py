9cea2459c9c298a77bb678aef8106637a98aff89
TolibjonDev <stolibjon123@gmail.com>
2023-12-13 20:21:20 +0500

    all

--- core/settings.py
+++ core/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # other middlewares...
-    "django_user_agents.middleware.UserAgentMiddleware",
 ]
 
 
